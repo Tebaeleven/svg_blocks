caf3026adfce21e13c79b23ad8fe3b6f7724c513
nano <nano@nanonoMacBook-Air.local>
2023-01-21 16:45:14 +0900

    svgの移動と拡大縮小ができるように

--- src/index.js
+++ src/index.js
@@ -4,24 +4,34 @@
 let s = Snap('#svg');
 
 // クローン用のsvg要素
-let cloneSvg = s.rect(10, 10, 100, 40, 8, 8);
+let cloneSvg = s.rect(10, 10, 25, 15, 8, 8);
 cloneSvg.attr({ stroke: '#f00', strokeWidth: 2, fill: '#f00', "fill-opacity": 0.5 });
 
 let count = 0
-let newSvgsArray = []
+let SvgArray = []
+let SvgXY = []
 let x1 = 5
 let y1 = 0
 
 let mouseX = 0
 let mouseY = 0
 
+let cameraX = 0
+let cameraY = 0
+let mouseDown = false
+let zoom = 1
+let modifiedCameraX = 0
+let modifiedCameraY = 0
 cloneSvg.click(function () {
     alert(this.data("count"));
 });
 
 // ボタンがクリックされた時の処理
 document.getElementById("add_btn").addEventListener("click", function () {
-    makeClone()
+    makeClone(0,0)
+    makeClone(100, 0)
+    makeClone(0, 100)
+    makeClone(100, 100)
     // newSvg.click(function () {
     //     alert(this.data("count"));
     // });
@@ -31,48 +41,77 @@ document.getElementById("move").addEventListener("click", function () {
     moveClone()
 })
 
-document.getElementById("speed").addEventListener("click", function () {
-    x1 += 10
-    y1 += 10
-    console.log(x1)
+document.getElementById("x+").addEventListener("click", function () {
+    moveClone()
 })
-
-function makeClone() {
+function makeClone(x,y) {
+    // let x = Math.random() * (s.node.clientWidth - newSvg.getBBox().width);
+    // let y = Math.random() * (s.node.clientHeight - newSvg.getBBox().height);
     //クローンを作成
-    let newSvg = cloneSvg.clone(); 
+    let newSvg = cloneSvg.clone();
     // クローンを追加
-    s.append(newSvg); 
-    // ランダムな座標を取得
-    let x = Math.random() * (s.node.clientWidth - newSvg.getBBox().width);
-    let y = Math.random() * (s.node.clientHeight - newSvg.getBBox().height);
-
-    // ランダムな座標に移動
-    newSvg.transform('t' + x + ',' + y);
+    s.append(newSvg);
+    newSvg.transform('t' + ((x)) + ',' + ((y)));
     newSvg.data("count", count)
+    SvgArray.push(newSvg);
+    let localX = newSvg.transform().localMatrix.e;
+    let localY = newSvg.transform().localMatrix.f;
+    SvgXY.push({ x: localX, y: localY });
+    console.log(SvgXY)
     count++;
-    newSvg.drag()
-    newSvgsArray.push(newSvg);
+    
+    moveClone()
+    console.log(count)
 }
 
 function moveClone() {
-    for (let i = 0; i < newSvgsArray.length; i++) {
-        let localX = newSvgsArray[i].transform().localMatrix.e;
-        let localY = newSvgsArray[i].transform().localMatrix.f;
-        newSvgsArray[i].transform('T' + (localX + mouseX) + ',' + (localY + mouseY));
+    for (let i = 0; i < SvgArray.length; i++) {
+        let localX = SvgXY[i].x;
+        let localY = SvgXY[i].y;
+        SvgArray[i].transform('T' + (localX * zoom + cameraX * zoom) + ',' + (localY * zoom + cameraY * zoom));
     }
 }
 
-document.addEventListener('mousedown', function (event) {
-    console.log('x: ' + mouseX + ', y: ' + mouseY);
-    mouseX = -(event.clientX/100)
-    mouseY = -(event.clientY / 100)
 
-});
+document.addEventListener("mousedown", function () {
+    mouseDown = true;
+})
+document.addEventListener("mouseup", function () {
+    mouseDown = false;
+})
+// document.addEventListener('mousemove', function (event) {
+//     if (mouseDown){
+//         console.log('x: ' + mouseX + ', y: ' + mouseY);
+//         mouseX = event.clientX - 300
+//         mouseY = event.clientY - 300
+//         moveClone(mouseX, mouseY)
+//     }
+
+// });
 
 //TODO ブロックをスクロールできるようにする。htmlにスクロールバーを追加し、デバッグしやすくする
+const inputElem = document.getElementById('x');
+inputElem.addEventListener('input', rangeOnChange);
+
+function rangeOnChange(e) {
+    console.log(e.target.value);
+    cameraX = Number(e.target.value)
+    moveClone()
+}
+
+const zoomElem = document.getElementById('zoom');
+zoomElem.addEventListener('input', zoomOnChange);
+
+function zoomOnChange(e) {
+    zoom = e.target.value
+    console.log(zoom);
+    moveClone()
+}
 
 //TODO オリジナルを隠すorオリジナルを作成しないようにする
 
 //TODO ボタンを押すとその位置からクローンを選択した状態でエディタ画面におけるようにする
     //TODO Drag関数を自作した方が良いかもしれない
     //(エディタ画面に置かれたクローンのドラッグ、ボタンからクローンされた時のドラッグの挙動を実装した方がわかりやすいかも)
+
+
