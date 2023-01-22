4f69bfdcc3166bbf8ed619f2421eeecb4ac10999
nano <nano@nanonoMacBook-Air.local>
2023-01-22 18:25:37 +0900

    cloneをdrag&dropできるように

--- src/test.js
+++ src/test.js
@@ -8,7 +8,10 @@ let cameraY = 0
 let svgArray = []
 let SvgXY = []
 drawXYAxis()
-
+let drawArea = 500
+// for (let i = 0; i <200; i++) {
+//     makeClone(getRandomArbitrary(-drawArea, drawArea), getRandomArbitrary(-drawArea, drawArea))
+// }
 makeClone(0, 0)
 makeClone(100, 0)
 makeClone(0, 100)
@@ -33,6 +36,7 @@ function makeClone(x, y) {
     svgArray.push(clone);
     original.parentNode.appendChild(clone);
     original.style.opacity = 0.5
+
     moveClone()
 
 }
@@ -41,12 +45,12 @@ function moveClone() {
     for (let i = 0; i < svgArray.length; ++i) {
         let localX = SvgXY[i].x
         let localY = SvgXY[i].y
-        let HalfWidth = getWidth(svgArray[i])/2
+        let HalfWidth = getWidth(svgArray[i]) / 2
         let HalfHeight = getHeight(svgArray[i]) / 2
         let g = svgArray[i].getElementsByTagName("g")
-        g[0].setAttribute("transform", "scale("+zoom+")")
-        svgArray[i].setAttribute("x", ((localX - cameraX) * zoom) + 250 - HalfWidth*zoom - 5*zoom)
-        svgArray[i].setAttribute("y", ((localY - cameraY) * zoom) + 250 - HalfHeight*zoom - 5*zoom)
+        g[0].setAttribute("transform", "scale(" + zoom + ")")
+        svgArray[i].setAttribute("x", ((localX - cameraX) * zoom) + 250 - HalfWidth * zoom - 5 * zoom)
+        svgArray[i].setAttribute("y", ((localY - cameraY) * zoom) + 250 - HalfHeight * zoom - 5 * zoom)
         // svgArray[i].setAttribute("transform", "scale(3)");
     }
 }
@@ -61,9 +65,9 @@ function getHeight(clone) {
     let height = rect[0].getAttribute("height")
     return Number(height)
 }
-function setWidth(clone,width) {
+function setWidth(clone, width) {
     let rect = clone.getElementsByTagName("rect")
-    rect[0].setAttribute("width",width)
+    rect[0].setAttribute("width", width)
 }
 function setHeight(clone, height) {
     let rect = clone.getElementsByTagName("rect")
@@ -120,9 +124,43 @@ const zoomElem = document.getElementById('zoom');
 zoomElem.addEventListener('input', zoomOnChange);
 
 function zoomOnChange(e) {
-    zoom = clamp(Number(e.target.value),0.1,6)
+    zoom = clamp(Number(e.target.value), 0.1, 5)
     moveClone()
 }
 function clamp(value, min, max) {
     return Math.min(Math.max(value, min), max);
 }
+svgArray.forEach(function (clone, index) {
+    clone.addEventListener("mousedown", function (event) {
+        // ドラッグ開始時のマウス位置
+        var startX = event.clientX;
+        var startY = event.clientY;
+        var elementX = SvgXY[index].x
+        var elementY = SvgXY[index].y
+        
+        document.addEventListener("mousemove", moveElement);
+        // mouseupイベントを追加
+        document.addEventListener("mouseup", stopDrag);
+        function moveElement(event) {
+            // 現在のマウス位置
+            var currentX = event.clientX;
+            var currentY = event.clientY;
+            // ドラッグした距離
+            var dx = currentX - startX;
+            var dy = currentY - startY;
+            // 要素の新しい位置
+            var newX = elementX + dx/zoom;
+            var newY = elementY + dy/zoom;
+            // 要素の位置を更新
+            SvgXY[index].x = newX
+            SvgXY[index].y = newY
+            moveClone()
+
+        }
+        function stopDrag() {
+            document.removeEventListener("mousemove", moveElement);
+            document.removeEventListener("mouseup", stopDrag);
+        }
+        // alert("clone clicked!" + index);
+    });
+});
